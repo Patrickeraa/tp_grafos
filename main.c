98968c476afc421863a84f17b543c351b9279d58
Marcos <aurelio.marcos@ufv.br>
2022-10-15 10:40:59 -0300

    Testes no arquivo main
    * chamei a função para ler do arquivo
    * imprimi o grafo e depois o limpei da memória

--- main.c
+++ main.c
@@ -1,30 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "functions.h"
-#include "functions.c"
+#include "Library.h"
+
+Grafo grafo;
 
 int main() {
-    char path[250] = "./inputs/grafo.txt";
-    float **grafo = inicializaGrafo(path);
-    int ordem = ordemGrafo(path);
-    for (int i= 0; i<5; i++){
-        if (i ==0){
-            printf("");
-        }
-        else{
-            printf("\n");
-        }
-        for (int k = 0; k<5; k++){
-            printf("%.1f ", grafo[i][k]);
-        }
-    }
-    int tamanho = tamanhoGrafo(grafo, ordem);
-    printf("\n%d", tamanho);
 
-    int **num_vizinhos = vizinhoVertice(grafo, ordem, 1);
-    printf("\n%d", num_vizinhos[1]);
+    leGrafo(&grafo); 
+    imprimeGrafo(&grafo);
+    liberaGrafo(&grafo);
+    // float **grafo = inicializaGrafo(path);
+    // int ordem = ordemGrafo(path);
+    // for (int i= 0; i<5; i++){
+    //     if (i ==0){
+    //         printf("");
+    //     }
+    //     else{
+    //         printf("\n");
+    //     }
+    //     for (int k = 0; k<5; k++){
+    //         printf("%.1f ", grafo[i][k]);
+    //     }
+    // }
+    // int tamanho = tamanhoGrafo(grafo, ordem);
+    // printf("\n%d", tamanho);
 
-    int grau = grauVertice(grafo, ordem, 1);
-    printf("\n%d", grau);
+    // int **num_vizinhos = vizinhoVertice(grafo, ordem, 1);
+    // printf("\n%d", num_vizinhos[1]);
+
+    // int grau = grauVertice(grafo, ordem, 1);
+    // printf("\n%d", grau);
     return 0;
 }
